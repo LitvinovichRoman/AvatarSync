152f90aa4088b980208467e6b3345ee6d28f5f43
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2024-08-13 11:16:35 +0300

    Project created

---
+++ AvatarSync/AvatarSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvatarSync
+//
+//  Created by Roman Litvinovich on 13.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
