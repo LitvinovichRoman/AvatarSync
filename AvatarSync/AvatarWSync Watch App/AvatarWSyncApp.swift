70c2c34deca6c0ee20557ada051c52c8afc02d8f
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2024-08-13 11:22:48 +0300

    Watch app connected

---
+++ AvatarSync/AvatarWSync Watch App/AvatarWSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvatarWSyncApp.swift
+//  AvatarWSync Watch App
+//
+//  Created by Roman Litvinovich on 13.08.24.
+//
+
+import SwiftUI
+
+@main
+struct AvatarWSync_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
