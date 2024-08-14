02400d6f7f731646a6a2b026de63e60e6cef280f
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2024-08-14 20:17:58 +0300

    App/Fetures: Updated AppDelegate.swift to work with WatchConnectivity

--- AvatarSync/AvatarSync/Application/AppDelegate.swift
+++ AvatarSync/AvatarSync/Application/AppDelegate.swift
@@ -6,31 +6,49 @@
 //
 
 import UIKit
+import WatchConnectivity
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        setUpWatchConnectivity()
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func setUpWatchConnectivity() {
+        if WCSession.isSupported() {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
     }
 
-
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
 }
 
+extension AppDelegate: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: (any Error)?) {
+        if let error =  error {
+            fatalError("Can't activate session with error : \(error.localizedDescription)")
+        }
+        
+        print("WC Session activates with state : \(activationState.rawValue) ")
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        print(#function)
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        print(#function)
+        WCSession.default.activate()
+    }
+    
+    
+}
